7724a812e529c8adeb910420f5a94e101780eca9
John Kelly <kellyj58@tcd.ie>
2016-06-23 01:29:47 +0100

    Removed commented code

--- String Concatenation V Template Strings/index.js
+++ String Concatenation V Template Strings/index.js
@@ -21,20 +21,3 @@ function template() {
 function templateWStatic() {
   var str = `test${b}hello`;
 }
-
-// var loops = 1000000;
-// console.info("Running test %d times", loops);
-// var times = {
-
-// };
-
-// var slowest;
-// var fastest;
-// // for(time in times) {
-// //   if(!slowest || times[time] > times[slowest]) slowest = time;
-// //   if(!fastest || times[time] < times[fastest]) fastest = time;
-// // }
-
-// // console.info("Slowest: %s", slowest);
-// // console.info("Fastest: %s", fastest);
-// writeResults();
